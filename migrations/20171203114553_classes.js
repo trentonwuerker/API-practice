838d3db71ba1c8efa96407aded9353fd536ef94c
Trenton Wuerker <trenton.wuerker@gmail.com>
2017-12-03 11:55:44 -0700

    started migration

--- migrations/20171203114553_classes.js
+++ migrations/20171203114553_classes.js
@@ -1,8 +1,11 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("classes", (classes) => {
+    classes.increments('id').unsigned().primary()
+    classes.string('name').notNull()
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('products')
 };
