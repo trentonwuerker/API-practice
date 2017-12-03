c6021b46f2f8f33e4eb03bf04586060ace272b7e
Trenton Wuerker <trenton.wuerker@gmail.com>
2017-12-03 11:29:13 -0700

    added knex

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/hi', (req, res) => {
+  res.render('layout', { title: 'Not Express' })
+})
+
 module.exports = router;
