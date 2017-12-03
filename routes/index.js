ce17795798dcd9d25c2d71331669f67c6d148d76
Trenton Wuerker <trenton.wuerker@gmail.com>
2017-12-03 11:40:58 -0700

    removed users file

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/hi', (req, res) => {
-  res.render('layout', { title: 'Not Express' })
-})
-
 module.exports = router;
