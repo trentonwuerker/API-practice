0802a08caa071ea03600135b69daa3ede4b8134c
Trenton Wuerker <trenton.wuerker@gmail.com>
2017-12-03 12:18:07 -0700

    added and seeded classes

--- migrations/20171203114553_classes.js
+++ migrations/20171203114553_classes.js
@@ -7,5 +7,5 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('products')
+  return knex.schema.dropTable('classes')
 };
--- seeds/classes.js
+++ seeds/classes.js
@@ -1,13 +1,45 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex('classes').del().then(function() {
+    // Inserts seed entries
+    return knex('classes').insert([
+      {
+        id: 1,
+        name: 'barbarian'
+      }, {
+        id: 2,
+        name: 'bard'
+      }, {
+        id: 3,
+        name: 'cleric'
+      }, {
+        id: 4,
+        name: 'druid'
+      }, {
+        id: 5,
+        name: 'fighter'
+      }, {
+        id: 6,
+        name: 'monk'
+      }, {
+        id: 7,
+        name: 'paladin'
+      }, {
+        id: 8,
+        name: 'ranger'
+      }, {
+        id: 9,
+        name: 'rogue'
+      }, {
+        id: 10,
+        name: 'sorcerer'
+      }, {
+        id: 11,
+        name: 'warlock'
+      }, {
+        id: 12,
+        name: 'wizard'
+      }
+    ]);
+  });
 };
