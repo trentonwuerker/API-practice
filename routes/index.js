4e0d0a06fa09479a0c912b3c4aa158d168fd9a8a
Trenton Wuerker <trenton.wuerker@gmail.com>
2017-12-03 17:51:19 -0700

    trying to fix knex

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var queries = require('../queries/queries')
+var knex = require('knex')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+//TODO extract method
+router.get('/api/', (req, res) => {
+  knex('classes').then((data) => {
+    res.json(data)
+  })
+})
 
 module.exports = router;
